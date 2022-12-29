6e534810b6143e21e3f1c1c9f6adb501bae0ca12
AliceChpmnk <alisablog50@gmail.com>
2022-12-29 23:09:21 +0200

    edits to task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,8 +6,8 @@ const createGalleryItems = (items) => {
     items.forEach(element => {
         itemsList.push(`
         <div class="gallery__item">
-            <a class="gallery__link" href="#">
-                <img class="gallery__image" src="${element.preview}" data-original="${element.original}" alt="${element.description}"></img>
+            <a class="gallery__link" href="${element.original}">
+                <img class="gallery__image" src="${element.preview}" data-source="${element.original}" alt="${element.description}"/>
             </a>
         </div>`)
     });
@@ -17,19 +17,30 @@ const createGalleryItems = (items) => {
 
 const onGalleryCardClick = event => {
     const { target } = event;
+    event.preventDefault();
 
     if (target.nodeName !== 'IMG') {
         return;
     }
-    
-    const targetImgUrl = target.dataset.original;
+
+    const targetImgUrl = target.dataset.source;
     const modal = basicLightbox.create(`
 	<img src="${targetImgUrl}"></img>
 `)
     modal.show();
+
+    // додаткове завдання
+    const closeModal = event => {
+        if (event.code === "Escape") {
+            modal.close();
+            document.removeEventListener("keydown", closeModal);
+        }
+    };
+        
+    document.addEventListener("keydown", closeModal);
 }
 
 const gallery = document.querySelector(".gallery");
 gallery.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
 
-gallery.addEventListener("click", onGalleryCardClick)
+gallery.addEventListener("click", onGalleryCardClick);
