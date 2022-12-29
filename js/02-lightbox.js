335b0de1afa02487e1014d142b153d6034fa0e08
AliceChpmnk <alisablog50@gmail.com>
2022-12-29 23:42:57 +0200

    second task is ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const createGalleryItems = (items) => {
+    const itemsList = [];
+    items.forEach(element => {
+        itemsList.push(`
+        <a class="gallery__item" href="${element.original}">
+            <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+        </a>`)
+    });
+    
+    return itemsList.join('');
+}
+
+const onGalleryCardClick = event => {
+    const { target } = event;
+    event.preventDefault();
+
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
+    lightbox.open();
+}
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+gallery.addEventListener("click", onGalleryCardClick);
