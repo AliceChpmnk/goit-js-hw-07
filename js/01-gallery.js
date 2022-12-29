0eb0d9edf2b3393a0e86ee6a5823089ffaeee026
AliceChpmnk <alisablog50@gmail.com>
2022-12-29 22:33:20 +0200

    first task is ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const createGalleryItems = (items) => {
+    const itemsList = [];
+    items.forEach(element => {
+        itemsList.push(`
+        <div class="gallery__item">
+            <a class="gallery__link" href="#">
+                <img class="gallery__image" src="${element.preview}" data-original="${element.original}" alt="${element.description}"></img>
+            </a>
+        </div>`)
+    });
+    
+    return itemsList.join('');
+}
+
+const onGalleryCardClick = event => {
+    const { target } = event;
+
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    const targetImgUrl = target.dataset.original;
+    const modal = basicLightbox.create(`
+	<img src="${targetImgUrl}"></img>
+`)
+    modal.show();
+}
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+gallery.addEventListener("click", onGalleryCardClick)
